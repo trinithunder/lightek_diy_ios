e3e6323de50a4b78e901c53c265cb0662510d4a0
Marlon <marlon@Marlons-MacBook-Pro.local>
2025-03-18 15:13:28 -0400

    I have xcode in here now thank God

---
+++ lightek-diy/lightek-diy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  lightek-diy
+//
+//  Created by Marlon on 3/18/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ lightek-diy/lightek-diy/lightek_diyApp.swift
@@ -0,0 +1,17 @@
+//
+//  lightek_diyApp.swift
+//  lightek-diy
+//
+//  Created by Marlon on 3/18/25.
+//
+
+import SwiftUI
+
+@main
+struct lightek_diyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
